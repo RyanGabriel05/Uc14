cf82800d64c51b65b1235649ccc5e86d45495aae
RyanGabriel05 <ryangabriel.angelo@gmail.com>
2022-05-17 16:06:07 -0300

    Encontro Remoto 1

---
+++ EncontroRemoto1/ChapterFST1/ChapterFST1/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using ChapterFST1.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterFST1.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _LivroRepository;
+
+        public LivroController(LivroRepository LivroRepository)
+        {
+            _LivroRepository = LivroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_LivroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
